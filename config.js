29f21193a548e5b8a174398d446a2a507d15e8da
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-11-30 18:58:47 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import moment from 'moment-timezone';
 
 global.owner = [
   ['50240289706', 'CREADOR', true],
-  ['50240289706', 'CREADOR', true],
+  ['59169776370', 'CREADOR', true],
   ['50258487658', 'CREADOR', true],
   ['50240289706', 'CREADOR', true]
   
