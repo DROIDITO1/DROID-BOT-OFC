35e5d09ecdcaa45ef3b8a6a09d54d7c1948d939f
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-12-01 20:29:00 -0600

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -2,13 +2,15 @@ import fs from "fs"
 let handler = m => m
 handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
-if (/^sticker|funciones|stiker$/i.test(m.text) && !chat.isBanned) { 
+if (/^Hola bot| HOLA BOT$/i.test(m.text) && !chat.isBanned) { 
 conn.sendPresenceUpdate('recording', m.chat)   
-await m.reply(`*Hola, mis funciones incluyen la creación de stickers, descargar música/vídeos, y entre otras más.*\n\n\nRecuerda que cada comando que pones debe ir primero un punto y luego el comando. Ejemplo:
- */s*\n\nSímbolos admitidos: * / ! " .  @ + × ^
-\nTambién registrate para que puedas ver todas mis funciones y usarlas. Espero serte útil.
-\nregistrate escribiendo: */reg nombre.edad*
-ejemplo: */reg droid.17*`)
+await m.reply(`*Hola, ¿en qué puedo ayudarte hoy?* 
+ Si eres un nuevo usuario, puedes registrarte en mi base de datos para aprovechar todos mis comandos.
+
+Puedo ayudarte a crear stickers, descargar videos o música, y responder cualquier tipo de pregunta. Para crear un sticker, usa el comando */s* y para ver mi menú de comandos, utiliza */menu*
+
+o sí quieres registrarte usa el siguiente comando 
+: */reg*   `)
 conn.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', seconds: '4556', ptt: true }, { quoted: m })
 }
 return !0
