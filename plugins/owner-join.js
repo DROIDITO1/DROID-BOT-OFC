fca1e9f1b7eec197ace5512753a55dc0f7746e00
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-12-21 21:54:38 -0600

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -1,22 +1,29 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
 
 let handler = async (m, { conn, text, isMods, isOwner, isPrems }) => {
-let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
-let [_, code] = link.match(linkRegex) || []
+    let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text;
+    let [_, code] = link.match(linkRegex) || [];
 
-if (!code) throw '*[ ⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] ERROR VUELVA A INTENTARLO VERIFIQUES QUE SU ENLACE NO ESTÉ VENCIDO UN EJEMPLO'
+    if (!code) throw '*[ ⚠️ 𝐀𝐋𝐄𝐑𝐓𝐀 ⚠️ ] ERROR VUELVA A INTENTARLO VERIFIQUES QUE SU ENLACE NO ESTÉ VENCIDO UN EJEMPLO';
 
-if ( isPrems || isMods || isOwner || m.fromMe) {
-let res = await conn.groupAcceptInvite(code)
-await m.reply(`*𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚄𝙽𝙸𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾 𝙰𝙻 𝙶𝚁𝚄𝙿𝙾, 𝙳𝙸𝚂𝙵𝚁𝚄𝚃𝙴 𝙳𝙴𝙻 𝙱𝙾𝚃! ✔️*\n\n\n THE BOT HAS JOINED SUCCESSFULLY ENJOY THE BOT✔️`)
-} else {
-const data = global.owner.filter(([id]) => id)
+    if (isPrems || isMods || isOwner || m.fromMe) {
+        let res = await conn.groupAcceptInvite(code);
+        await m.reply(`*𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚄𝙽𝙸𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾 𝙰𝙻 𝙶𝚁𝚄𝙿𝙾, 𝙳𝙸𝚂𝙵𝚁𝚄𝚃𝙴 𝙳𝙴𝙻 𝙱𝙾𝚃! ✔️*\n\n\n THE BOT HAS JOINED SUCCESSFULLY ENJOY THE BOT✔️`);
+    } else {
+        // Verificar si se proporcionan 5 diamantes
+        const diamonds = 5; // Número de diamantes requeridos
+        if (m.mentionedJidList.length >= diamonds) {
+            let res = await conn.groupAcceptInvite(code);
+            await m.reply(`*𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚄𝙽𝙸Ó 𝙳𝙴𝙻 𝙻𝙸𝙽𝙺 𝙿𝙾𝚁 𝙲𝙾𝙽𝙴𝚇𝙸𝙾𝙽 𝙳𝙴 ${diamonds} 𝙳𝙸𝙰𝙼𝙰𝙽𝚃𝙴𝚂 ✔️*\n\n\n THE BOT HAS JOINED SUCCESSFULLY ENJOY THE BOT✔️`);
+        } else {
+            await m.reply(`*[❗𝐈𝐍𝐅𝐎❗] SE REQUIEREN AL MENOS ${diamonds} DIAMANTES PARA QUE EL BOT SE UNA AL GRUPO [❗𝐈𝐍𝐅𝐎❗]*`);
+        }
+    }
+};
 
-for (let jid of data.map(([id]) => [id] + '@s.whatsapp.net').filter(v => v != conn.user.jid)) await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝚄𝙴𝚅𝙰 𝚂𝙾𝙻𝙸𝙲𝙸𝚃𝚄𝙳 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙿𝙰𝚁𝙰 𝚄𝙽 𝙶𝚁𝚄𝙿𝙾 [❗𝐈𝐍𝐅𝐎❗]*\n\n*—◉ 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴𝙻 𝚂𝙾𝙻𝙸𝙲𝙸𝚃𝙰𝙽𝚃𝙴:* ' + 'wa.me/' + m.sender.split('@')[0] + '\n*—◉ 𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 𝙳𝙾𝙽𝙳𝙴 𝚂𝙴 𝚂𝙾𝙻𝙸𝙲𝙸𝚃𝙰 𝙴𝙻 𝙱𝙾𝚃:*\n\n\n*[❗𝐈𝐍𝐅𝐎❗] BOT REQUEST FOR A GROUP [❗𝐈𝐍𝐅O❗]*\n\n*—◉ REQUEST NUMBER:* ' + 'wa.me/' + m.sender.split('@')[ 0] + '\n*—◉ LINK WHERE THE BOT IS REQUESTED*' + link, jid)
-await m.reply('*[[❗𝐈𝐍𝐅𝐎❗] SE HA MANDADO LA SOLICITUD AL PROPIETARIO DEL BOT*')}}
-handler.help = ['join [chat.whatsapp.com]']
-handler.tags = ['register']
-handler.command = /^join|nuevogrupo$/i
-handler.register = true
-handler.limit = 2
-export default handler
+handler.help = ['join [chat.whatsapp.com]'];
+handler.tags = ['register'];
+handler.command = /^join|nuevogrupo$/i;
+handler.register = true;
+handler.limit = 5;
+export default handler;
