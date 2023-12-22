dac1a2ad03d0b48a7fa22c3c67a93d596666b0d0
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-12-21 22:00:05 -0600

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -10,13 +10,14 @@ let handler = async (m, { conn, text, isMods, isOwner, isPrems }) => {
         let res = await conn.groupAcceptInvite(code);
         await m.reply(`*𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚄𝙽𝙸𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾 𝙰𝙻 𝙶𝚁𝚄𝙿𝙾, 𝙳𝙸𝚂𝙵𝚁𝚄𝚃𝙴 𝙳𝙴𝙻 𝙱𝙾𝚃! ✔️*\n\n\n THE BOT HAS JOINED SUCCESSFULLY ENJOY THE BOT✔️`);
     } else {
-        // Verificar si se proporcionan 5 diamantes
-        const diamonds = 5; // Número de diamantes requeridos
-        if (m.mentionedJidList.length >= diamonds) {
-            let res = await conn.groupAcceptInvite(code);
-            await m.reply(`*𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚄𝙽𝙸Ó 𝙳𝙴𝙻 𝙻𝙸𝙽𝙺 𝙿𝙾𝚁 𝙲𝙾𝙽𝙴𝚇𝙸𝙾𝙽 𝙳𝙴 ${diamonds} 𝙳𝙸𝙰𝙼𝙰𝙽𝚃𝙴𝚂 ✔️*\n\n\n THE BOT HAS JOINED SUCCESSFULLY ENJOY THE BOT✔️`);
+        const diamondsRequired = 5;
+
+        // Verificar si m.mentionedJidList está definido y tiene al menos 5 diamantes
+        if (m.mentionedJidList && m.mentionedJidList.length >= diamondsRequired) {
+            await conn.groupAcceptInvite(code);
+            await m.reply(`*𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚄𝙽𝙸Ó 𝙳𝙴𝙻 𝙻𝙸𝙽𝙺 𝙿𝙾𝚁 𝙲𝙾𝙽𝙴𝚇𝙸𝙾𝙽 𝙳𝙴 ${diamondsRequired} 𝙳𝙸𝙰𝙼𝙰𝙽𝚃𝙴𝚂 ✔️*\n\n\n THE BOT HAS JOINED SUCCESSFULLY ENJOY THE BOT✔️`);
         } else {
-            await m.reply(`*[❗𝐈𝐍𝐅𝐎❗] SE REQUIEREN AL MENOS ${diamonds} DIAMANTES PARA QUE EL BOT SE UNA AL GRUPO [❗𝐈𝐍𝐅𝐎❗]*`);
+            await m.reply(`*[❗𝐈𝐍𝐅𝐎❗] SE REQUIEREN AL MENOS ${diamondsRequired} DIAMANTES PARA QUE EL BOT SE UNA AL GRUPO [❗𝐈𝐍𝐅𝐎❗]*`);
         }
     }
 };
@@ -25,5 +26,5 @@ handler.help = ['join [chat.whatsapp.com]'];
 handler.tags = ['register'];
 handler.command = /^join|nuevogrupo$/i;
 handler.register = true;
-handler.limit = 5;
+handler.limit = 2;
 export default handler;
